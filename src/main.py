35ee61a5161dc485b7331bbc4cc79f38b53cff0e
Jose Lucas Silva Mayer <josemayer@usp.br>
2022-10-03 17:44:53 -0300

    Adiciona suporte a argumentos de ações do crawler
    Co-authored-by: Jose Lucas Silva Mayer <josemayer@usp.br>
    Co-authored-by: Marilia Dicezare <mariliatd@gmail.com>

--- src/main.py
+++ src/main.py
@@ -1,5 +1,22 @@
+import sys
 import data
 import plot
 
-estados = data.data_read("../data/states_incomplete.json")
-plot.amount_buses_per_time(estados)
+def main():
+    if len(sys.argv) < 0:
+        print("Usage:")
+        print(f"\tpython main.py <funcao> <argumentos>")
+    else:
+        if sys.argv[1] == "plot":
+            name_data = sys.argv[2]
+            estados = data.data_read(name_data)
+            plot.plot_buses(estados)
+        elif sys.argv[1] == "crawl":
+            name_data = sys.argv[2]
+            time = sys.argv[3]
+            interval = sys.argv[4]
+            url = sys.argv[5]
+            data.data_register(name_data, int(time), int(interval), url)
+
+if __name__ == "__main__":
+    main()
